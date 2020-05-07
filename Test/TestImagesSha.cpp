97ab9d40a639f758008253a63e77562c7a81cfe1
Joan Andrés <andres6936@live.com>
2020-05-07 13:48:49 -0500

    Implemented the read of sha256sum

--- Test/TestImagesSha.cpp
+++ Test/TestImagesSha.cpp
@@ -2,7 +2,9 @@
 
 #include <vector>
 #include <string>
+#include <utility>
 #include <iostream>
+#include <fstream>
 #include <filesystem>
 #include <boost/process.hpp>
 
@@ -10,11 +12,15 @@ namespace fs = std::filesystem;
 
 int main(int argc, char* argv[])
 {
+	using VectorString = std::vector<std::string>;
+
 	fs::path images = L"../Test/Images/";
 	fs::directory_entry directory(images);
 	std::vector<fs::path> filenames;
-	std::vector<fs::path> filesPNG;
+	std::vector<fs::path> filenamesPNG;
+	std::vector<fs::path> filenamesMD;
 	std::vector<std::string> filenameOutput;
+	std::pair<VectorString, VectorString> sha256sum;
 
 	if (directory.exists() and directory.is_directory())
 	{
@@ -27,16 +33,34 @@ int main(int argc, char* argv[])
 		{
 			if (file.extension() == ".png")
 			{
-				filesPNG.push_back(file);
+				filenamesPNG.push_back(file);
+				filenamesMD.push_back(fs::path(file).replace_extension(".md"));
 				filenameOutput.push_back(file.filename().string() + ".xp");
 			}
 		}
 
 		const std::string executable = "Gall";
 
-		for (int i = 0; i < filesPNG.size(); ++i)
+		for (int i = 0; i < filenamesPNG.size(); ++i)
 		{
-			boost::process::system(executable, filesPNG[i].string(), filenameOutput[i]);
+			boost::process::system(executable, filenamesPNG[i].string(), filenameOutput[i]);
+		}
+
+		for (const fs::path& file: filenamesMD)
+		{
+			std::ifstream reader(file);
+
+			if (reader.good())
+			{
+				std::string fileSha256sum;
+				reader >> fileSha256sum;
+				sha256sum.first.push_back(file.string());
+				sha256sum.second.push_back(fileSha256sum);
+			}
+			else
+			{
+				std::cerr << "Can not open the file " << file.string() << "\n";
+			}
 		}
 	}
 	else
